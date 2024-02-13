e4018d98777d653355afa78b1e9ee74057412b82
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2024-02-13 13:11:37 -0800

    Update 01_bedops.sh

--- pipeline/RNAseq/CLFT044/01_bedops.sh
+++ pipeline/RNAseq/CLFT044/01_bedops.sh
@@ -3,6 +3,6 @@
 
 module load bedops
 
-grep "scaffold_10" result/CCB1.bed > result/CCB1_int.bed
+grep "scaffold_10" coverage.res.tsv > scaffold_10_coverage.tsv
 awk -vFS=" " -vOFS=" " '{ print $1, $2, ($2 + 1), ".", $3 }' scaffold_10_coverage.tsv > test.bed
 bedops --merge test.bed | bedops --chop 10000 - | bedmap --echo --mean --delim ' ' - test.bed > answer.bed
